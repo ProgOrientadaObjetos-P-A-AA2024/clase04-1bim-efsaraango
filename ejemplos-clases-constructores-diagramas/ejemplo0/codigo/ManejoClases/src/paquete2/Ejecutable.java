373465bd0285ec907dad62981795152b8555e7c9
efsaraango <efas_24_69@hotmail.com>
2024-05-01 16:38:06 -0500

    Semana 4 Constructores

---
+++ ejemplos-clases-constructores-diagramas/ejemplo0/codigo/ManejoClases/src/paquete2/Ejecutable.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquete2;
+
+/**
+ *
+ * @author Det-Pc
+ */
+public class Ejecutable {
+    public static void main(String[] args) {
+        Computadora portatil = new Computadora("Intel-x19", 6.1+4.2);
+        System.out.printf("Datos \n%s, %.2f" , portatil.obtenerTipoProcesador(), portatil.obtenerMemoria());
+    }
+}
---
+++ ejemplos-clases-constructores-diagramas/ejemplo0/codigo/ManejoClases/src/paquete3/Principal.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquete3;
+
+/**
+ *
+ * @author Det-Pc
+ */
+public class Principal {
+    public static void main(String[] args) {
+        //Esta clase principal donde se encuentra el "main" estamos creando un
+        //objeto el cual esta haciendo uso del constructor vacio q esta de 
+        //de forma inplicita en la clase "Computador"
+        Computadora computadoraTrabajo = new Computadora();
+    }
+}
